ea80a539fd966106c800949f69b07bb56a5399bd
Christopher F <foxbot@protonmail.com>
2016-11-22 22:43:52 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# gpmdp-np
+### Google Play Music Desktop Player - Now Playing
+
+A tool to export the currently playing song (and relevant metadata) to a file
---
+++ gpmdp-np.go
@@ -0,0 +1,13 @@
+// GPMDP-NP
+//   Google Play Music Desktop Player - Now Playing
+// 
+// a tool to export the currently playing song (and relevant metadata)
+// to a file
+//
+// (c) 2016 foxbot
+// 
+// Licensed under ISC
+
+func main() {
+
+}
